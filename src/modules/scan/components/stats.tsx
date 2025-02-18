cfdc94d17efec04faad7a2bbd24fa3d1df11ee01
yosho <yleecruz@gmail.com>
2025-02-18 16:07:59 -0500

    pr api v1 added 18.02.252d33332

--- src/modules/scan/components/stats.tsx
+++ src/modules/scan/components/stats.tsx
@@ -10,14 +10,14 @@ export const Stats = ({ parcels }: { parcels: any[] }) => {
 	}, [parcels]);
 
 	return (
-		<Card className="grid mx-2 bg-muted h-16 gap-2 p-2 grid-cols-4">
+		<Card className="grid  bg-muted h-10 gap-2  grid-cols-4">
 			<div className="bg-secondary rounded-lg text-center flex flex-col justify-center items-center">
 				<span className="text-xs text-muted-foreground">Total</span>
 				<span className="text-sm font-bold">{stats.total}</span>
 			</div>
 			<div className="bg-secondary rounded-lg text-center flex flex-col justify-center items-center">
-				<span className="text-xs text-muted-foreground">Scanned</span>
-				<span className="text-sm font-bold">{stats.scanned}</span>
+				<span className="text-xs text-green-600">Scanned</span>
+				<span className="text-sm font-bold text-green-600">{stats.scanned}</span>
 			</div>
 			<div className="bg-secondary rounded-lg text-center flex flex-col justify-center items-center">
 				<span className="text-xs text-muted-foreground">Pending</span>
