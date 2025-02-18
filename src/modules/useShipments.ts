4384061bcb10c4f44ac09c59d8bc5834f4e70a8b
yosho <yleecruz@gmail.com>
2025-02-18 15:10:29 -0500

    pr api v1 added 18.02.252d

--- src/modules/useShipments.ts
+++ src/modules/useShipments.ts
@@ -1,10 +1,10 @@
 import { api } from "@/api/api";
-import { ShipmentInterface } from "@/api/api";
+import { ShipmentsInterface } from "@/api/api";
 import { useMutation } from "@tanstack/react-query";
 
 export const useUpsertShipment = () => {
 	const mutation = useMutation({
-		mutationFn: (shipments: ShipmentInterface[]) => api.shipments.upsert(shipments),
+		mutationFn: (shipments: ShipmentsInterface[]) => api.shipments.upsert(shipments),
 	});
 	return mutation;
 };
