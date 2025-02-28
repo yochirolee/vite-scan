94c94de60630c15a67192d8e5c7f3b700b425cde
yosho <yleecruz@gmail.com>
2025-02-28 12:34:41 -0500

    pr fixed some css issues on shipment-time-lime

--- src/components/common/getIcon.tsx
+++ src/components/common/getIcon.tsx
@@ -1,21 +1,38 @@
-import { CheckCircle, Truck, Clock, Shield, Forklift, Ship, Warehouse } from "lucide-react";
+import {
+	CheckCircle,
+	Truck,
+	Clock,
+	Shield,
+	Forklift,
+	Ship,
+	Warehouse,
+	PackageCheck,
+	Package,
+	BaggageClaim,
+} from "lucide-react";
 
 import { Anchor } from "lucide-react";
 
 export const getIcon = (code: string) => {
 	switch (code) {
+		case "CREATED":
+			return <Package className="text-emerald-500 h-4 w-4" />;
 		case "IN_WAREHOUSE":
 			return <Warehouse className="text-blue-500 h-4 w-4" />;
 		case "IN_CONTAINER":
-			return <Ship className="text-violet-500 h-4 w-4" />;
+			return <Ship className="text-orange-500 h-4 w-4" />;
 		case "IN_PORT":
 			return <Anchor className="text-violet-500 h-4 w-4" />;
 		case "CUSTOMS_PENDING":
 			return <Shield className="text-yellow-500 h-4 w-4" />;
 		case "READY_FOR_PICKUP":
 			return <Forklift className="text-gray-500 h-4 w-4" />;
+		case "MESSENGER_RECEIVED":
+			return <PackageCheck className="text-lime-500 h-4 w-4" />;
 		case "IN_TRANSIT":
-			return <Truck className="text-purple-500 h-4 w-4" />;
+			return <Truck className="text-sky-500 h-4 w-4" />;
+		case "OUT_FOR_DELIVERY":
+			return <BaggageClaim className="text-sky-500 h-4 w-4" />;
 		case "DELIVERED":
 			return <CheckCircle className="text-green-500 h-4 w-4" />;
 		default:
--- src/components/shipments/shipment-timeline-item.tsx
+++ src/components/shipments/shipment-timeline-item.tsx
@@ -18,6 +18,7 @@ interface TimelineItemProps {
 		country_code: string;
 		city: string;
 	};
+
 	isLast: boolean;
 }
 
@@ -31,7 +32,7 @@ export default function ShipmentTimelineItem({
 	isLast,
 }: TimelineItemProps) {
 	return (
-		<div className="flex  items-center mb-8 w-full">
+		<div className="flex h-16  items-center mb-8 w-full">
 			<div className="flex items-center relative">
 				<div
 					className={`w-6 h-6 rounded-full  flex items-center justify-center z-10
@@ -41,7 +42,7 @@ export default function ShipmentTimelineItem({
 				</div>
 				{!isLast && (
 					<div
-						className={`h-[49px] w-[0.5px]  absolute top-[28px] left-3 -translate-x-1/2
+						className={`h-[60px] flex-1 w-[0.6px]  absolute top-[30px] left-3 -translate-x-1/2
               ${isCompleted ? "bg-green-500" : "bg-gray-300/20"}`}
 					></div>
 				)}
@@ -51,18 +52,16 @@ export default function ShipmentTimelineItem({
 					<div className="flex flex-row justify-between w-full">
 						<h3
 							className={`font-semibold text-xs ${
-								isCompleted ? "text-green-500" : "text-gray-500"
+								isCompleted ? "text-green-500" : "text-gray-300"
 							}`}
 						>
 							{status?.name}
 						</h3>
-						<span className="text-[10px]  text-gray-400">{user?.name}</span>
+						<span className="text-[10px]  text-gray-600">{user?.name}</span>
 					</div>
 					<p className="text-sm text-gray-600">{status?.description}</p>
-					<p className="text-xs  text-gray-400">
-						{location?.state} {location?.city}
-					</p>
-					<span className="text-xs text-gray-400">
+
+					<span className="text-xs mt-1 text-gray-400">
 						{new Date(timestamp).toLocaleDateString("en-US", {
 							day: "numeric",
 							month: "short",
@@ -71,6 +70,9 @@ export default function ShipmentTimelineItem({
 							minute: "2-digit",
 						})}
 					</span>
+					<p className="text-xs mt-1  text-gray-500">
+						{location?.state} {location?.city}
+					</p>
 				</div>
 			</div>
 		</div>
